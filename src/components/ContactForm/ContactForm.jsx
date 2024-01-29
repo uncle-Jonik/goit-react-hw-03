fa7cc386e408cd4b7ebd5ff0aacdc579d39f5a4f
uncle-Jonik <Katsimon_Eu@outlook.com>
2024-01-29 14:35:57 +0200

    add validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,6 +4,11 @@ import { nanoid } from 'nanoid';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
+const SignupSchema = Yup.object().shape({
+  name: Yup.string().min(3, 'Too Short!').max(50, 'Too Long!').required('Required'),
+  number: Yup.number().max(7, 'Too Long!').positive().integer().required('Required'),
+});
+
 export const ContactForm = ({ onAdd }) => {
   const lableName = useId();
   const lableNumber = useId();
@@ -11,6 +16,7 @@ export const ContactForm = ({ onAdd }) => {
   return (
     <Formik
       initialValues={{ name: '', number: '' }}
+      validationSchema={SignupSchema}
       onSubmit={(value, actions) => {
         onAdd({ id: nanoid(), ...value });
         actions.resetForm();
@@ -19,9 +25,11 @@ export const ContactForm = ({ onAdd }) => {
       <Form className={css.formicForm}>
         <label htmlFor={lableName}>Name:</label>
         <Field type="text" id={lableName} name="name" />
+        <ErrorMessage name="name" />
 
         <label htmlFor={lableNumber}>Number:</label>
         <Field type="text" id={lableNumber} name="number" />
+        <ErrorMessage name="number" />
 
         <button type="submit">Add contact</button>
       </Form>
